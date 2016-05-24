c36c0b0e2c84df93959e0c3921c1895e188c2428
Fernando Villarroel <fenavillarroel@gmail.com>
2016-05-24 00:17:36 -0300

    Primera versión

---
+++ controllers/default.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    #response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+@auth.requires_login()
+def lista():
+   
+    r=request.args
+    if r:
+        l=db.executesql('select * from lista where id=%s' % r[0])[0]
+        i=l[0]
+        n=l[1]
+    else:
+        i,n='',''
+
+
+    #if form.accepts(request.vars,session):
+    if request.vars:
+
+        try:
+            db.executesql("update lista set nombre='%s' where id=%s" % (request.vars.nombre,request.vars.id))
+            db.commit()
+        except:
+            pass
+        redirect(URL('rlistas'))
+
+    return dict(i=i,n=n)
+
+@auth.requires_login()
+def addcampain():
+
+    l=db.executesql('select nombre,id from lista where id_clte=%s order by nombre asc' % auth.user.id)
+    d={}
+    for i in l:
+        d[i[1]]=i[0]
+
+    form=SQLFORM.factory(Field('nombre','string',length=56,requires=IS_NOT_EMPTY()),
+                        Field('fecha','datetime',requires=IS_DATETIME(format='%Y-%m-%d %H:%M:%S'),default=datetime.datetime.now()),
+                        Field('lista','integer',requires=IS_IN_SET(d),represent= lambda value, row: d[value]),
+                        Field('estado','boolean',default='f'),formstyle='bootstrap',_class="form-group")
+
+    if form.process().accepted:
+
+        if form.vars.estado:
+            e='T'
+        else:
+            e='F'
+        db.executesql("insert into campain (nombre,fecha,id_clte,id_lista,estado) values ( \
+                        '%s','%s',%s,%s,'%s')" % (form.vars.nombre,form.vars.fecha,auth.user.id,form.vars.lista,e))
+        db.commit()
+
+        session.flash = T("Campaña Correctamente Creada!!!")
+
+        redirect(URL('campain'))
+
+    return dict(form=form)
+    
+@auth.requires_login()
+def addlist():
+
+    if request.vars:
+
+        db.executesql("insert into lista (nombre,id_clte) values ('%s',%s)" % (request.vars.nombre,auth.user.id))
+        db.commit()
+        l=db.executesql("select currval('lista_id_seq')")[0][0]
+        session.flash = T("Lista Correctamente Creada!!!")
+
+        redirect(URL('uploadfile',args=[auth.user.id,l]))
+
+    return locals()
+
+@auth.requires_login()
+def rlistas():
+
+    rws=db.executesql('select * from lista where id_clte=%s order by id desc' % (auth.user.id,))
+
+    return dict(rws=rws)
+
+
+@auth.requires_login()
+def campain():
+
+    rws  =  db.executesql('select id,nombre,fecha,estado,  \
+                                (select count(*) as total from contactos  \
+                                join lista on campain.id_lista=lista.id    \
+                                where campain.id_clte=%s),   \
+                                (select count(*) as pendiente from contactos \
+                                join lista on campain.id_lista=lista.id \
+                                join estadosms on estadosms.id=contactos.estado  \
+                                where campain.id_clte=%s and estadosms.id=1), \
+                                (select count(*) as cola from contactos \
+                                join lista on campain.id_lista=lista.id \
+                                join estadosms on estadosms.id=contactos.estado  \
+                                where campain.id_clte=%s and estadosms.id=2), \
+                                (select count(*) as enviados from contactos \
+                                join lista on campain.id_lista=lista.id \
+                                join estadosms on estadosms.id=contactos.estado  \
+                                where campain.id_clte=%s and estadosms.id=3), \
+                                (select count(*) as entregados from contactos \
+                                join lista on campain.id_lista=lista.id \
+                                join estadosms on estadosms.id=contactos.estado  \
+                                where campain.id_clte=%s and estadosms.id=4), \
+                                (select count(*) as fallidos from contactos \
+                                join lista on campain.id_lista=lista.id \
+                                join estadosms on estadosms.id=contactos.estado  \
+                                where campain.id_clte=%s and estadosms.id=5) \
+                            from campain    \
+                            where id_clte=%s    \
+                            order by id desc' % (auth.user.id,auth.user.id,auth.user.id,auth.user.id,auth.user.id,auth.user.id,auth.user.id))
+
+    return dict(rws=rws)
+
+
+@auth.requires_login()
+def updatecampain():
+
+    c=request.args[0]
+
+    l=db.executesql('select nombre,id from lista where id_clte=%s order by nombre asc' % auth.user.id)
+    d={}
+    for i in l:
+        d[i[1]]=i[0]
+    dat=db.executesql('select * from campain where id=%s' % (c,))[0]
+    cam=dat[4]
+    if dat[5]=='T' or dat[5]=='t':
+        v=True
+    else:
+        v=False
+
+    form=SQLFORM.factory(Field('nombre','string',length=56,requires=IS_NOT_EMPTY(),default=dat[1]),
+                        Field('fecha','datetime',requires=IS_DATETIME(format='%Y-%m-%d %H:%M:%S'),default=dat[2]),
+                        Field('lista','integer',requires=IS_IN_SET(d),default=cam),
+                        Field('estado','boolean',default=v),formstyle='bootstrap',_class="form-group")
+
+
+
+    if form.process().accepted:
+
+        if form.vars.estado:
+            e='T'
+        else:
+            e='F'
+
+        db.executesql("update campain set nombre='%s', fecha='%s',id_lista=%s,estado='%s' where id=%s" % (form.vars.nombre,form.vars.fecha,form.vars.lista,e,c))
+        db.commit()
+        session.flash = T("Campaña Actuaizada!!!")
+
+        redirect(URL('campain'))
+
+
+    return dict(form=form)
+
+@auth.requires_login()
+def contactos():
+
+    idlista=request.args[0]
+
+    rws=db.executesql('select contactos.id,contactos.numero,contactos.msg,estadosms.nombre from contactos \
+                        join estadosms on estadosms.id=contactos.estado \
+                        where contactos.id_lista=%s \
+                        order by contactos.id desc' % (idlista,))
+
+
+    return dict(rws=rws)
+
+@auth.requires_login()
+def uploadfile():
+
+    id_user,lista=request.args[0],request.args[1]
+
+    form=SQLFORM.factory(Field('Archivo','upload',custom_store=store_file),formstyle='bootstrap',_class="form-group")
+    #form.process(formname='form')
+    form1=SQLFORM.factory(Field('Mensaje','text',requires=IS_NOT_EMPTY()),
+                        Field('Archivo','upload',custom_store=store_file),formstyle='bootstrap',_class="form-group")
+    #form1.process(formname='form1')
+
+    if form.accepts(request.vars, session,formname='form'):
+            with open('applications/smscenter/uploads/'+form.vars.Archivo,'rb') as fin: 
+            # csv.DictReader uses first line in file for column headings by default
+                dr = csv.reader(fin) # comma is default delimiter
+                for row in dr:
+
+                    db.executesql("insert into contactos (numero,msg,id_lista) values ('%s','%s',%s)" % (row[0],row[1],lista))
+                    db.commit()
+            
+            os.system('rm %s'% 'applications/smscenter/uploads/'+form.vars.Archivo)
+            session.flash = T("Contactos Insertados Correctamente!!!")
+
+            redirect(URL('rlistas'))
+
+
+    if form1.accepts(request.vars, session,formname='form1'):
+            with open('applications/smscenter/uploads/'+form1.vars.Archivo,'rb') as fin: 
+            # csv.DictReader uses first line in file for column headings by default
+                dr = csv.reader(fin) # comma is default delimiter
+                for row in dr:
+                    db.executesql("insert into contactos (numero,msg,id_lista) values ('%s','%s',%s)" % (row[0],form1.vars.Mensaje,lista))
+                    db.commit()
+            os.system('rm %s'% 'applications/smscenter/uploads/'+form1.vars.Archivo)
+            session.flash = T("Contactos y Mensaje Manual Correctamente Insertados!!!")
+
+            redirect(URL('rlistas'))
+
+    return dict(form=form,form1=form1)
+
+def store_file(file,filename=None,path=None):
+    import os
+    import shutil
+    path="applications/smscenter/uploads/"
+    #if not os.path.exists(path):
+    #    os.makedirs(path)
+    pathfilename = os.path.join(path, filename)
+    dest_file = open(pathfilename, 'wb')
+    try:
+        shutil.copyfileobj(file, dest_file)
+    finally:
+        dest_file.close()
+    return filename
+
+def retrieve_file(filename, path=None):
+    path = "uploads/"
+    return (filename, open(os.path.join(path, filename), 'rb'))
---
+++ models/db.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+if request.global_settings.web2py_version < "2.14.1":
+    raise HTTP(500, "Requires web2py 2.13.3 or newer")
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+## app configuration made easy. Look inside private/appconfig.ini
+from gluon.contrib.appconfig import AppConfig
+## once in production, remove reload=True to gain full speed
+myconf = AppConfig(reload=True)
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    #db = DAL(myconf.get('db.uri'), 
+    #         pool_size = myconf.get('db.pool_size'),
+    #         migrate_enabled = myconf.get('db.migrate'),
+    #         check_reserved = ['all'])
+    db = DAL('postgres://sms:vmo123@127.0.0.1:5432/sms')
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore+ndb')
+    ## store sessions and tickets there
+    session.connect(request, response, db=db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## choose a style for forms
+response.formstyle = myconf.get('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
+response.form_label_separator = myconf.get('forms.separator') or ''
+
+
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+## (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+
+from gluon.tools import Auth, Service, PluginManager
+
+# host names must be a list of allowed host names (glob syntax allowed)
+auth = Auth(db, host_names=myconf.get('host.names'))
+service = Service()
+plugins = PluginManager()
+
+## create all tables needed by auth if not custom tables
+#auth.define_tables(username=False, signature=False)
+
+auth.settings.extra_fields['auth_user']= [
+    Field('nombres', length=128, default=''),
+    Field('rut', length=28, default=''),
+    Field('direccion', length=128, default=''),
+    Field('email', length=128, default='', unique=True), # required
+    Field('password', 'password', length=512,            # required
+        readable=False, label='Password'),
+    Field('giro',length=128, default=''),
+    Field('prepago','boolean', default='t',notnull=True),
+    Field("saldo", "integer", notnull=True, label='Fallidos',default=0,writable=False),
+    Field('telefono',length=128, default=''),
+    Field('registration_key', length=512,                # required
+        writable=False, readable=False, default=''),
+    Field('reset_password_key', length=512,              # required
+        writable=False, readable=False, default=''),
+    Field('registration_id', length=512,                 # required
+        writable=False, readable=False, default='')]
+
+auth.define_tables(username=False, signature=False)
+
+##configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' if request.is_local else myconf.get('smtp.server')
+mail.settings.sender = myconf.get('smtp.sender')
+mail.settings.login = myconf.get('smtp.login')
+mail.settings.tls = myconf.get('smtp.tls') or False
+mail.settings.ssl = myconf.get('smtp.ssl') or False
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+## after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
+db.define_table("gateway",
+    Field("nombre", "string", length=56, notnull=True, label="Gateway Nombre", writable=True),
+    Field("url", "string", length=256, notnull=True, label="URL", writable=True),
+    Field("estado", "boolean", notnull=True, label="Estado Gateway", default='t', writable=True), format='%(nombre)s',migrate=True)
+
+db.define_table("estadosms",
+    Field("nombre","string", length=56,notnull=True,default=None, label="Estado SMS",writable=True),format='%(nombre)s',migrate=True)
+
+db.define_table("lista",
+    Field("nombre","string", length=56,notnull=True,default=None, label="Nombre Lista",writable=True),
+    Field("id_clte",db.auth_user, notnull=True,writable=False),format='%(nombre)s',migrate=True)
+
+db.define_table("contactos",
+    Field("numero","string", length=56, notnull=True, default=None, label="Numero", writable=True),
+    Field("msg","string", length=140, notnull=True, default='', label="Texto Mensaje", writable=True),
+    Field("gw",db.gateway, notnull=False, label="Gateway",writable=False),
+    Field("id_lista",db.lista, notnull=True, label="Lista",writable=False),
+    Field("estado",db.estadosms, notnull=True,default=1,writable=False),migrate=True)
+
+db.contactos.id_lista.requires = IS_IN_DB(db,db.lista.id,'%(nombre)s')
+db.contactos.estado.requires = IS_IN_DB(db,db.estadosms.id,'%(nombre)s')
+
+db.define_table("campain",
+   Field("nombre", "string", length=56, notnull=True, default=None, label='Nombre',writable=True),
+   Field("fecha", "datetime", notnull=True,label='Fecha Inicio',default=datetime.datetime.now(),writable=True),
+#   Field("Total", "integer", notnull=True, label='Total',default=0,writable=False),
+#   Field("Pendientes", "integer", notnull=True, label='Pendientes',default=0,writable=False),
+#   Field("Cola", "integer", notnull=True, label='Enviados',default=0,writable=False),
+#   Field("Enviados", "integer", notnull=True, label='Enviados',default=0,writable=False),
+#   Field("Entregados", "integer", notnull=True, label='Entregados',default=0,writable=False),
+#   Field("Fallidos", "integer", notnull=True, label='Fallidos',default=0,writable=False),
+   Field("id_clte", db.auth_user, notnull=True,writable=False),
+   Field("id_lista", db.lista, notnull=True,writable=True),
+   Field("estado", "boolean", notnull=True, label='Estado', default='t',writable=True),format='%(nombre)s',migrate=True)
+
+db.campain.id_lista.requires = IS_IN_DB(db,db.lista.id,'%(nombre)s')
+db.campain.id_clte.requires = IS_IN_DB(db,db.auth_user.id,'%(nombres)s')
+
+db.define_table("tarifas",
+    Field("nombre", length=56, notnull=True,default=None, label="Nombre Tarifa", writable=True), format='%(nombre)s',migrate=True)
+
+db.define_table("prefix",
+    Field("prefix", "string", length=56, notnull=True, label="Prefijo", writable=True),
+    Field("tarifa", db.tarifas, notnull=True, label="Nombre Tarifa", writable=True),
+    Field("nombre", "string", length=56, notnull=True, label="Nombre Prefijo", writable=True),  format='%(nombre)s',migrate=True)
+
+db.prefix.tarifa.requires = IS_IN_DB(db,db.tarifas.id,'%(nombre)s')
+
+
+
+
+
+
